0b797ae83fd052b0e0b741ad420a36bcbf0b0a5e
Connor Gaspar <c5gaspar@uwaterloo.ca>
2017-02-02 18:29:14 -0500

    Project Completed
    Changes included a proper reordering of the factor levels of the
    distDat$Angle variable. Inclusion of a vanilla R script, and a final
    knit of the project.
    RPub - http://rpubs.com/c5gaspar/P372-A1

--- Assignment1.R
+++ Assignment1.R
@@ -3,17 +3,15 @@
 #### University of Waterloo, February 2017
 
 
-
-```r
+##### SETUP
 set.seed(372)
 library(xlsx); library(ggplot2); library(ggthemes); library(psych)
 library(ez); library(BayesFactor); library(knitr)
 ## Reading raw data
 datS <- read.xlsx("p372 class data assignment 1.xlsx", sheetName="Sheet1", stringsAsFactors=F)
-```
 
 
-```r
+##### DATA MANIPULATION
 ## Organizing raw data
 datS[c(41, 82),] <- NA # Removing header rows from data
 datS1 <- datS[,1:5] 
@@ -43,26 +41,17 @@ anglDat <- distDat[distDat$Angle %in% seq(0, 180, 45),] # Excluding trials where
 
 anglDat$Angle <- as.factor(anglDat$Angle)
 anglDat$Angle <- factor(anglDat$Angle, levels=c(0, 45, 90, 135, 180)) #Reordering
-```
 
 
 
-```r
+##### PLOTTING
 ## Plotting distributions
 hist(dat$Distance, breaks = seq(0, 700, 10), col="red",
      main="Pre-outlier exclusion distribution", xlab = "Distance (cm)") # Pre-outlier analysis
-```
 
-![plot of chunk Plotting](figure/Plotting-1.png)
-
-```r
 hist(distDat$Distance, breaks = seq(0, 250, 10), col="red",
      main="Post-outlier exclusion distribution", xlab="Distance (cm)") # Post-outlier analysis
-```
 
-![plot of chunk Plotting](figure/Plotting-2.png)
-
-```r
 ## Creating distance plot data #
 distPlotDat1 <- aggregate(Distance ~ Sex+Position, data=distDat, mean)
 distPlotDat1$SD <- aggregate(Distance ~ Sex+Position, data=distDat, sd)[,"Distance"]
@@ -112,50 +101,42 @@ for(i in 1:nrow(anglPlotDat1)){
 
 # Plotting Distance w.r.t Position
 ggplot(data=distPlotDat3, aes(x=Position, y=Distance, fill=Position))+
-               geom_bar(stat="identity", position=position_dodge(.7), colour="black",
-                        size=.2, width=.7)+
-               geom_errorbar(aes(ymin=Lower, ymax=Upper),
-                             position=position_dodge(.9), width=.2)+
-               scale_y_continuous(breaks=seq(0, 60, 10), limits=c(0,60),
-                                  expand=c(0,0))+
-               labs(x="Pair Position",
-                    y="Distance from Partner (cm)")+
-               theme_base()+
-               scale_fill_brewer(palette="Dark2")+
-               theme(plot.margin=unit(c(.5,.25,.25,.5), "cm"),
-                     axis.title.y=element_text(margin=margin(0,15,0,0)),
-                     legend.title=element_blank())
-```
+        geom_bar(stat="identity", position=position_dodge(.7), colour="black",
+                 size=.2, width=.7)+
+        geom_errorbar(aes(ymin=Lower, ymax=Upper),
+                      position=position_dodge(.9), width=.2)+
+        scale_y_continuous(breaks=seq(0, 60, 10), limits=c(0,60),
+                           expand=c(0,0))+
+        labs(x="Pair Position",
+             y="Distance from Partner (cm)")+
+        theme_base()+
+        scale_fill_brewer(palette="Dark2")+
+        theme(plot.margin=unit(c(.5,.25,.25,.5), "cm"),
+              axis.title.y=element_text(margin=margin(0,15,0,0)),
+              legend.title=element_blank())
 
-![plot of chunk Plotting](figure/Plotting-3.png)
-
-```r
 # Plotting Distance w.r.t. Sex
 ggplot(data=distPlotDat2, aes(x=Sex, y=Distance, fill=Sex))+
-               geom_bar(stat="identity", position=position_dodge(), colour="black",
-                        size=.2)+
-               geom_errorbar(aes(ymin=Lower, ymax=Upper),
-                             position=position_dodge(.9), width=.2)+
-               scale_y_continuous(breaks=seq(0, 60, 10), limits=c(0,60),
-                                  expand=c(0,0))+
-               labs(x="Group Gender Pairs",
-                    y="Distance from Partner (cm)")+
-               theme_base()+
-               scale_fill_brewer(palette="Set1")+
-               theme(plot.margin=unit(c(.5,.25,.25,.5), "cm"),
-                     axis.title.y=element_text(margin=margin(0,15,0,0)),
-                     legend.title=element_blank())
-```
+        geom_bar(stat="identity", position=position_dodge(), colour="black",
+                 size=.2)+
+        geom_errorbar(aes(ymin=Lower, ymax=Upper),
+                      position=position_dodge(.9), width=.2)+
+        scale_y_continuous(breaks=seq(0, 60, 10), limits=c(0,60),
+                           expand=c(0,0))+
+        labs(x="Group Gender Pairs",
+             y="Distance from Partner (cm)")+
+        theme_base()+
+        scale_fill_brewer(palette="Set1")+
+        theme(plot.margin=unit(c(.5,.25,.25,.5), "cm"),
+              axis.title.y=element_text(margin=margin(0,15,0,0)),
+              legend.title=element_blank())
 
-![plot of chunk Plotting](figure/Plotting-4.png)
-
-```r
 # Plotting Distance w.r.t. Position grouped by Sex 
 ggplot(data=distPlotDat1, aes(x=Position, y=Distance, fill=Sex))+
         geom_bar(stat="identity", position=position_dodge(), colour="black",
                  size=.2)+
         geom_errorbar(aes(ymin=Lower, ymax=Upper),
-                          position=position_dodge(.9), width=.2)+
+                      position=position_dodge(.9), width=.2)+
         scale_y_continuous(breaks=seq(0, 80, 10), limits=c(0, 80), 
                            expand=c(0,0))+
         labs(x="Group Gender Pairs",
@@ -165,11 +146,7 @@ ggplot(data=distPlotDat1, aes(x=Position, y=Distance, fill=Sex))+
         theme(plot.margin=unit(c(.5,.25,.25,.5), "cm"),
               axis.title.y=element_text(margin=margin(0,15,0,0)),
               legend.title=element_blank())
-```
 
-![plot of chunk Plotting](figure/Plotting-5.png)
-
-```r
 # Plotting Distance w.r.t. Angle
 ggplot(data=anglPlotDat1, aes(x=Angle, y=Distance, fill=Angle))+
         geom_bar(stat="identity", position=position_dodge(), colour="black",
@@ -177,137 +154,46 @@ ggplot(data=anglPlotDat1, aes(x=Angle, y=Distance, fill=Angle))+
         geom_errorbar(aes(ymin=Lower, ymax=Upper),
                       position=position_dodge(.9), width=.2)+
         scale_y_continuous(breaks=seq(0, 100, 10), limits=c(0, 100),
-                         expand=c(0,0))+
+                           expand=c(0,0))+
         labs(x="Angle between Partners", y="Distance from Partner (cm)")+
         theme_base()+
         scale_fill_brewer(type="seq", palette="Greens")+
         theme(plot.margin=unit(c(.5,.25,.25,.5), "cm"),
               axis.title.y=element_text(margin=margin(0,15,0,0)),
               legend.title=element_blank())
-```
-
-![plot of chunk Plotting](figure/Plotting-6.png)
 
 
-```r
+##### QUICK STATS
 ## Difference in Distance BY Sex
 
 # Men -- Women
 t.test(distDat[distDat$Sex=="Men", "Distance"],
        distDat[distDat$Sex=="Women", "Distance"],
        paired=F)
-```
 
-```
-## 
-## 	Welch Two Sample t-test
-## 
-## data:  distDat[distDat$Sex == "Men", "Distance"] and distDat[distDat$Sex == "Women", "Distance"]
-## t = -0.029698, df = 101.74, p-value = 0.9764
-## alternative hypothesis: true difference in means is not equal to 0
-## 95 percent confidence interval:
-##  -12.70454  12.32973
-## sample estimates:
-## mean of x mean of y 
-##  45.76087  45.94828
-```
-
-```r
 ttestBF(distDat[distDat$Sex=="Men", "Distance"],
-       distDat[distDat$Sex=="Women", "Distance"],
-       paired=F)
-```
-
-```
-## Bayes factor analysis
-## --------------
-## [1] Alt., r=0.707 : 0.2084944 ±0.02%
-## 
-## Against denominator:
-##   Null, mu1-mu2 = 0 
-## ---
-## Bayes factor type: BFindepSample, JZS
-```
-
-```r
+        distDat[distDat$Sex=="Women", "Distance"],
+        paired=F)
 # Men -- Mixed
 t.test(distDat[distDat$Sex=="Men", "Distance"],
        distDat[distDat$Sex=="Mixed", "Distance"],
        paired=F)
-```
 
-```
-## 
-## 	Welch Two Sample t-test
-## 
-## data:  distDat[distDat$Sex == "Men", "Distance"] and distDat[distDat$Sex == "Mixed", "Distance"]
-## t = 0.46923, df = 102.8, p-value = 0.6399
-## alternative hypothesis: true difference in means is not equal to 0
-## 95 percent confidence interval:
-##  -9.564783 15.493301
-## sample estimates:
-## mean of x mean of y 
-##  45.76087  42.79661
-```
-
-```r
 ttestBF(distDat[distDat$Sex=="Men", "Distance"],
-       distDat[distDat$Sex=="Mixed", "Distance"],
-       paired=F)
-```
+        distDat[distDat$Sex=="Mixed", "Distance"],
+        paired=F)
 
-```
-## Bayes factor analysis
-## --------------
-## [1] Alt., r=0.707 : 0.2276216 ±0.03%
-## 
-## Against denominator:
-##   Null, mu1-mu2 = 0 
-## ---
-## Bayes factor type: BFindepSample, JZS
-```
-
-```r
 ## Difference in Distance BY Position
 
 # Standing -- Seated
 t.test(distDat[distDat$Position=="Standing", "Distance"],
        distDat[distDat$Position=="Sitting", "Distance"],
        paired=F)
-```
 
-```
-## 
-## 	Welch Two Sample t-test
-## 
-## data:  distDat[distDat$Position == "Standing", "Distance"] and distDat[distDat$Position == "Sitting", "Distance"]
-## t = -0.45533, df = 128.2, p-value = 0.6496
-## alternative hypothesis: true difference in means is not equal to 0
-## 95 percent confidence interval:
-##  -13.715353   8.583774
-## sample estimates:
-## mean of x mean of y 
-##  43.68421  46.25000
-```
-
-```r
 ttestBF(distDat[distDat$Sex=="Men", "Distance"],
-       distDat[distDat$Sex=="Mixed", "Distance"],
-       paired=F)
-```
+        distDat[distDat$Sex=="Mixed", "Distance"],
+        paired=F)
 
-```
-## Bayes factor analysis
-## --------------
-## [1] Alt., r=0.707 : 0.2276216 ±0.03%
-## 
-## Against denominator:
-##   Null, mu1-mu2 = 0 
-## ---
-## Bayes factor type: BFindepSample, JZS
-```
-
-```r
 ####
 
 ## Two-way ANOVAs 
@@ -319,105 +205,17 @@ ezANOVA(data=distDat,
         wid=Unique,
         between=.(Position, Sex),
         detailed=T)
-```
 
-```
-## $ANOVA
-##         Effect DFn DFd       SSn      SSd         F         p p<.05
-## 1     Position   1 157  284.8680 186028.3 0.2404166 0.6245900      
-## 2          Sex   2 157  379.3706 186028.3 0.1600864 0.8522091      
-## 3 Position:Sex   2 157 4721.6444 186028.3 1.9924343 0.1397963      
-##           ges
-## 1 0.001528974
-## 2 0.002035167
-## 3 0.024753063
-## 
-## $`Levene's Test for Homogeneity of Variance`
-##   DFn DFd      SSn      SSd        F         p p<.05
-## 1   5 157 4435.989 126450.2 1.101541 0.3618346
-```
-
-```r
 anovaBF(Distance ~ Position*Sex, data=distDat, progress=F)
-```
 
-```
-## Bayes factor analysis
-## --------------
-## [1] Position                      : 0.1895959   ±0%
-## [2] Sex                           : 0.07038591  ±0.02%
-## [3] Position + Sex                : 0.01314613  ±1.65%
-## [4] Position + Sex + Position:Sex : 0.007130708 ±3.16%
-## 
-## Against denominator:
-##   Intercept only 
-## ---
-## Bayes factor type: BFlinearModel, JZS
-```
-
-```r
 # Distance ~ Position*Angle
 ezANOVA(data=anglDat,
         dv=Distance,
         wid=Unique,
         between=.(Position, Angle),
         detailed=T)
-```
 
-```
-## $ANOVA
-##           Effect DFn DFd       SSn      SSd         F           p p<.05
-## 1       Position   1 145   519.087 166803.9 0.4512342 0.502818296      
-## 2          Angle   4 145 16501.488 166803.9 3.5861211 0.008060148     *
-## 3 Position:Angle   4 145  4455.350 166803.9 0.9682413 0.426917452      
-##           ges
-## 1 0.003102305
-## 2 0.090021845
-## 3 0.026015236
-## 
-## $`Levene's Test for Homogeneity of Variance`
-##   DFn DFd     SSn      SSd         F         p p<.05
-## 1   9 145 4523.25 112693.8 0.6466598 0.7555617
-```
-
-```r
 anovaBF(Distance ~ Position*Angle, data=anglDat, progress=F)
-```
 
-```
-## Bayes factor analysis
-## --------------
-## [1] Position                          : 0.185753  ±0%
-## [2] Angle                             : 8.018534  ±0.04%
-## [3] Position + Angle                  : 1.703854  ±1%
-## [4] Position + Angle + Position:Angle : 0.4225701 ±1.63%
-## 
-## Against denominator:
-##   Intercept only 
-## ---
-## Bayes factor type: BFlinearModel, JZS
-```
-
-```r
 # Distance ~ Angle regression
 lm(Distance ~ as.numeric(Angle), data=anglDat)
-```
-
-```
-## 
-## Call:
-## lm(formula = Distance ~ as.numeric(Angle), data = anglDat)
-## 
-## Coefficients:
-##       (Intercept)  as.numeric(Angle)  
-##            61.592             -5.411
-```
-
-
-```r
-knit2html("Assignment1.Rmd", quiet = T)
-```
-
-```
-## Error in parse_block(g[-1], g[1], params.src): duplicate label 'Setup'
-```
